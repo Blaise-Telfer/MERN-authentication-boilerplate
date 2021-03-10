5d6012293917e67f6f67366919295abbc2135cbd
Blaise Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2021-03-09 20:56:23 -0800

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -5,28 +5,27 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
 	username: {
-		type: String,
-		required: true,
-		unique: true
+	    type: String,
+	    required: true,
+	    unique: true
 	},
 	email: {
-		type: String,
-		required: true,
-		unique: true
+	    type: String,
+	    required: true,
+	    unique: true
 	},
 	password: {
-		type: String,
-		required: true
+	    type: String,
+	    required: true
 	},
 	role: {
-		type: String,
-		default: "user"
+	    type: String,
+	    default: "user"
 	},
 	date: {
-		type: Date,
-		default: Date.now
+	    type: Date,
+	    default: Date.now
 	}
 });
-
 
 module.exports = mongoose.model("User", UserSchema);
