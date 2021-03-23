8d59aa51b2a696920ba6d927122a715acf93bc50
Blaise Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2021-03-23 06:49:07 -0700

    expanded model
    extra queries

--- models/userModel.js
+++ models/userModel.js
@@ -5,26 +5,38 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
 	username: {
-	    type: String,
-	    required: true,
-	    unique: true
+	  type: String,
+	  required: true,
+	  unique: true
 	},
 	email: {
-	    type: String,
-	    required: true,
-	    unique: true
+	  type: String,
+	  required: true,
+	  unique: true
 	},
 	password: {
-	    type: String,
-	    required: true
+	  type: String,
+	  required: true
 	},
 	role: {
-	    type: String,
-	    default: "user"
+	  type: String,
+	  default: "user"
+	},
+	verified: {
+	  type: Boolean,
+	  default: false
+	},
+	verifyToken: {
+	  token: String,
+	  expires: Date
+	},
+	resetToken: {
+	  token: String,
+	  expires: Date
 	},
 	date: {
-	    type: Date,
-	    default: Date.now
+	  type: Date,
+	  default: Date.now
 	}
 });
 
